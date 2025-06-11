ccdb4e294efae2d7cd8b59f22bd4b159ac4740d7
Michelle Dang <michelledang@Michelles-MacBook-Air-2.local>
2025-06-11 01:56:30 -0700

    theme toggle

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/* theme toggle */}
+        <ThemeToggle/>
+
+        {/* backgroun effects */}
+
+        {/* nav bar */}
+
+        {/* Main Content */}
+
+        {/* footer */}
+    </div>;
+};
