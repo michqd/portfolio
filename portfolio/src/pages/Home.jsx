629415e09dcbc5ed14dcae8fb9c08eb4e335383c
Michelle Dang <michelledang@Michelles-MacBook-Air-2.local>
2025-06-20 14:45:42 -0700

    added more features and designs

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -7,8 +9,10 @@ export const Home = () => {
         <ThemeToggle/>
 
         {/* backgroun effects */}
+        <StarBackground/> 
 
         {/* nav bar */}
+        <Navbar/>
 
         {/* Main Content */}
 
